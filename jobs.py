60ba3f09c46cacb7fb68edd0925046d5ac6f7918
franklinmartin <84481627+franklinmartin@users.noreply.github.com>
2024-04-18 03:39:32 -0400

    update jobs.py

--- jobs.py
+++ jobs.py
@@ -1,3 +1,5 @@
+import sys
+
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
@@ -6,7 +8,30 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 from selenium.webdriver.common.keys import Keys
-url = "https://www.ziprecruiter.com/jobs-search?&search=software+intern&location=Los+Angeles%2C+CA&page="
+
+search_job_title = sys.argv[1]
+search_job_title = search_job_title.split(" ")
+search_location = sys.argv[2]
+search_location = search_location.split(",")
+search_location_city = search_location[0].strip()
+search_location_state = search_location[1].strip()
+
+def assign_url(search_job_title, search_location_city, search_location_state):
+    sjt = search_job_title[0]
+    for i in range(1, len(search_job_title)):
+        if i == len(search_job_title):
+            break
+        sjt += "+" + search_job_title[i]
+    url = "https://www.ziprecruiter.com/jobs-search?search="
+    url += sjt + "&location=" + search_location_city + "%2C+" + search_location_state + "&page="
+    return url
+
+sjt = assign_url(search_job_title=search_job_title,
+                 search_location_city=search_location_city,
+                 search_location_state=search_location_state)
+url = sjt
+#url = "https://www.ziprecruiter.com/jobs-search?&search=software&location=Los+Angeles%2C+CA&page="
+#url = "https://www.ziprecruiter.com/jobs-search?search=software&location=remote&page="
 profile_path = "C:\\Users\Hal\\Documents\\GitHub\\jobs-auto-apply\\profile\\aauwq980.default-release"
 
 options = webdriver.FirefoxOptions()
@@ -25,7 +50,7 @@ def click(element):
     #element = driver.find_element_by_xpath(xpath)
     driver.execute_script("arguments[0].click();", element)
 
-for j in range(1, 100):
+for j in range(1, 100000):
     driver.get( url + str(j))
     apply_buttons = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "text-button-secondary-default-text")))
     for i in range(0, len(apply_buttons)):
