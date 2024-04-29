adca20713565439e62368afce5b02807e66435bd
franklinmartin <84481627+franklinmartin@users.noreply.github.com>
2024-04-29 15:28:37 -0400

    add test.bat

---
+++ test - original.bat
@@ -0,0 +1,16 @@
+START /B /wait .\jobs_original.py "software"  "Miami, FL"
+START /B /wait .\jobs_original.py "software intern"  "Miami, FL"
+START /B /wait .\jobs_original.py "computer science"  "Miami, FL"
+
+START /B /wait .\jobs_original.py "software"  "Remote, (USA)"
+START /B /wait .\jobs_original.py "software"  "San Francisco, CA"
+START /B /wait .\jobs_original.py "software"  "Los Angeles, CA"
+
+
+START /B /wait .\jobs_original.py "software intern"  "Remote, (USA)"
+START /B /wait .\jobs_original.py "software intern"  "San Francisco, CA"
+START /B /wait .\jobs_original.py "software intern"  "Los Angeles, CA"
+
+START /B /wait .\jobs_original.py "computer science"  "Remote, (USA)"
+START /B /wait .\jobs_original.py "computer science"  "San Francisco, CA"
+START /B /wait .\jobs_original.py "computer science"  "Los Angeles, CA"
---
+++ test.bat
@@ -0,0 +1,16 @@
+START /B /wait .\jobs_functions.py "software"  "Miami, FL" 90
+START /B /wait .\jobs_functions.py "software intern"  "Miami, FL" 90
+START /B /wait .\jobs_functions.py "computer science"  "Miami, FL" 90
+
+START /B /wait .\jobs_functions.py "software"  "Remote, (USA)" 90
+START /B /wait .\jobs_functions.py "software"  "San Francisco, CA" 90
+START /B /wait .\jobs_functions.py "software"  "Los Angeles, CA" 90
+
+
+START /B /wait .\jobs_functions.py "software intern"  "Remote, (USA)" 90
+START /B /wait .\jobs_functions.py "software intern"  "San Francisco, CA" 90
+START /B /wait .\jobs_functions.py "software intern"  "Los Angeles, CA" 90
+
+START /B /wait .\jobs_functions.py "computer science"  "Remote, (USA)" 90
+START /B /wait .\jobs_functions.py "computer science"  "San Francisco, CA" 90
+START /B /wait .\jobs_functions.py "computer science"  "Los Angeles, CA" 90
